846db52a2eb82446eaf7a83ee2b8414badb3ef91
rafaela-css <rafaela-css@hotmail.com>
2022-07-15 12:46:52 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+
+public class PrimeiroPrograma {
+
+
+    public static void main(String[] args) {
+         int a= 5;
+        int b= 3;
+
+        System.out.println("Hello World!" + (a+b));
+    }
+
+}
